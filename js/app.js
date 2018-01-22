504e55781db03ac1869815840ac13ad50cf2f5b4
Jeffery Graham <jeffery.lemar.graham@gmail.com>
2018-01-22 15:32:07 -0500

    Make list of cards static

--- js/app.js
+++ js/app.js
@@ -1,8 +1,11 @@
 /*
  * Create a list that holds all of your cards
  */
-const cards = document.getElementsByClassName('card');
-
+const cards = ["fa fa-diamond", "fa fa-paper-plane-o", "fa fa-anchor",
+               "fa fa-bolt", "fa fa-cube", "fa fa-anchor", "fa fa-leaf",
+               "fa fa-bicycle", "fa fa-diamond", "fa fa-bomb", "fa fa-leaf",
+               "fa fa-bomb", "fa fa-bolt", "fa fa-bicycle",
+               "fa fa-paper-plane-o", "fa fa-cube"];
 
 /*
  * Display the cards on the page
@@ -10,6 +13,7 @@ const cards = document.getElementsByClassName('card');
  *   - loop through each card and create its HTML
  *   - add each card's HTML to the page
  */
+
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
